eef18234b32468a7761687d9a1697fe4d9689532
kothamasa_sreekanth <ksreekanth703@gmail.com>
2025-02-18 21:31:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React, { useState, useEffect } from 'react';
 import CountryCard from './CountryCard';
 import './App.css';
@@ -6,7 +5,6 @@ import './App.css';
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -15,22 +13,16 @@ const App = () => {
         const response = await fetch("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries");
         if (!response.ok) throw new Error('Failed to fetch data');
         const data = await response.json();
-
-        console.log('Fetched Data:', data); 
-        
         setCountries(data);
       } catch (error) {
         console.error('Error fetching data:', error);
         setError('Could not fetch countries.');
-      } finally {
-        setLoading(false);
       }
     };
   
     fetchCountries();
   }, []);
   
-
   const filteredCountries = countries.filter((country) =>
     country.common.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -45,26 +37,21 @@ const App = () => {
         className="searchBar"
       />
 
-      {loading && <p>Loading...</p>}
       {error && <p className="error">{error}</p>}
 
       <div className="countryContainer">
         {filteredCountries.length > 0 ? (
-          filteredCountries.map((country) => {
-            console.log('Rendering country:', country); 
-            return (
-              <CountryCard 
-                key={country.cca3} 
-                name={country.common || 'Unknown'} 
-                flag={country.png || ''}
-              />
-            );
-          })
+          filteredCountries.map((country) => (
+            <CountryCard 
+              key={country.cca3} 
+              name={country.common || 'Unknown'} 
+              flag={country.png || ''}
+            />
+          ))
         ) : (
           <p>No matching countries found.</p>
         )}
       </div>
-
     </div>
   );
 };
